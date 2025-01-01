9864046f9fcf4e5b260b5955a2e4dda56d7e926c
samarpitabdas <samarpitabdas@gmail.com>
2025-01-01 20:03:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from '../src/components/navbar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      < Navbar />
+    </div>
+  );
+}
+
+export default App;
